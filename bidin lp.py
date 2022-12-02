01e83f33bfea4bfd95654f1041d17e92f35dd4c2
Ipang07 <118873438+Ipang07@users.noreply.github.com>
2022-12-02 20:46:42 +0800

    Add files via upload

---
+++ bidin lp.py
@@ -0,0 +1,219 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: G:\sijil ii ku\projek ask akhir tahun.py ==============
+
+**********************************************
+	Menu Mengira Isi padu dan Luas
+**********************************************
+1. Luas
+2. Isipadu
+3. Tamat
+
+**********************************************
+Pilihan anda [1 - 3] : ___ 1
+
+**********************************************
+Menu Mengira Luas
+**********************************************
+1. Segi empat tepat
+2. Segi tiga
+3. Trapezium
+4. Bulatan
+
+**********************************************
+Pilihan anda [1 - 4] : ___ 1
+Masukkan panjang:2
+Masukkan lebar:3
+Luas ialah 6 
+
+
+**********************************************
+	Menu Mengira Isi padu dan Luas
+**********************************************
+1. Luas
+2. Isipadu
+3. Tamat
+
+**********************************************
+Pilihan anda [1 - 3] : ___ 1
+
+**********************************************
+Menu Mengira Luas
+**********************************************
+1. Segi empat tepat
+2. Segi tiga
+3. Trapezium
+4. Bulatan
+
+**********************************************
+Pilihan anda [1 - 4] : ___ 2
+Masukkan tapak:4
+Masukkan tinggi:5
+Luas ialah 10.0 
+
+
+**********************************************
+	Menu Mengira Isi padu dan Luas
+**********************************************
+1. Luas
+2. Isipadu
+3. Tamat
+
+**********************************************
+Pilihan anda [1 - 3] : ___ 1
+
+**********************************************
+Menu Mengira Luas
+**********************************************
+1. Segi empat tepat
+2. Segi tiga
+3. Trapezium
+4. Bulatan
+
+**********************************************
+Pilihan anda [1 - 4] : ___ 3
+Masukkan panjang sisi pertama:5
+Masukkan panjang sisi kedua:6
+Masukkan tinggi:7
+Luas ialah 38.5 
+
+
+**********************************************
+	Menu Mengira Isi padu dan Luas
+**********************************************
+1. Luas
+2. Isipadu
+3. Tamat
+
+**********************************************
+Pilihan anda [1 - 3] : ___ 1
+
+**********************************************
+Menu Mengira Luas
+**********************************************
+1. Segi empat tepat
+2. Segi tiga
+3. Trapezium
+4. Bulatan
+
+**********************************************
+Pilihan anda [1 - 4] : ___ 4
+Masukkan jejari:7
+Luas ialah 153.958 
+
+
+**********************************************
+	Menu Mengira Isi padu dan Luas
+**********************************************
+1. Luas
+2. Isipadu
+3. Tamat
+
+**********************************************
+Pilihan anda [1 - 3] : ___ 2
+
+**********************************************
+Menu Mengira Isi padu
+**********************************************
+1. Kuboid
+2. Silinder
+3. Kon
+4. Sfera
+
+**********************************************
+Pilihan anda [1 - 4] : ___ 1
+Masukkan panjang:4
+Masukkan tapak:5
+Masukkan tinggi:6
+Isi padu ialah 62500 
+
+
+**********************************************
+	Menu Mengira Isi padu dan Luas
+**********************************************
+1. Luas
+2. Isipadu
+3. Tamat
+
+**********************************************
+Pilihan anda [1 - 3] : ___ 2
+
+**********************************************
+Menu Mengira Isi padu
+**********************************************
+1. Kuboid
+2. Silinder
+3. Kon
+4. Sfera
+
+**********************************************
+Pilihan anda [1 - 4] : ___ 2
+Masukkan jejari:5
+Masukkan tinggi:6
+Isi padu ialah 471.29999999999995 
+
+
+**********************************************
+	Menu Mengira Isi padu dan Luas
+**********************************************
+1. Luas
+2. Isipadu
+3. Tamat
+
+**********************************************
+Pilihan anda [1 - 3] : ___ 2
+
+**********************************************
+Menu Mengira Isi padu
+**********************************************
+1. Kuboid
+2. Silinder
+3. Kon
+4. Sfera
+
+**********************************************
+Pilihan anda [1 - 4] : ___ 3
+Masukkan jejari:5
+Masukkan tinggi:6
+Isi padu ialah 157.09999999999997 
+
+
+**********************************************
+	Menu Mengira Isi padu dan Luas
+**********************************************
+1. Luas
+2. Isipadu
+3. Tamat
+
+**********************************************
+Pilihan anda [1 - 3] : ___ 2
+
+**********************************************
+Menu Mengira Isi padu
+**********************************************
+1. Kuboid
+2. Silinder
+3. Kon
+4. Sfera
+
+**********************************************
+Pilihan anda [1 - 4] : ___ 4
+Masukkan jejari:89
+Isi padu ialah 2953350.130666666 
+
+
+**********************************************
+	Menu Mengira Isi padu dan Luas
+**********************************************
+1. Luas
+2. Isipadu
+3. Tamat
+
+**********************************************
+Pilihan anda [1 - 3] : ___ 3
+
+**********************************************
+Terima Kasih kerana menggunakan atur cara ini.
+
+**********************************************
